5c4896b04e3566c3c18191a0a883c44b80a8eec9
Junior Samaroo <jns0987654321@gmail.com>
2017-03-26 22:13:21 -0400

    Forecast class and downloading data complete

--- rainyshinycloudyapp/Constants.swift
+++ rainyshinycloudyapp/Constants.swift
@@ -17,3 +17,5 @@ let API_KEY = "7a46805e7f029c1f0a3916f78e7d5beb"
 typealias DownloadComplete = () -> ()
 
 let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)-36\(LONGITUDE)123\(APP_ID)\(API_KEY)"
+
+let FORECAST_URL = "http://samples.openweathermap.org/data/2.5/forecast/daily?lat=-36&lon=123&cnt=10&appid=7a46805e7f029c1f0a3916f78e7d5beb"
